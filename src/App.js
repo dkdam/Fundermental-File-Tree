9be8d57f2d4499a5c14b240857885e0dc0ce88b3
dkdam <dkdam@live.com>
2022-11-22 20:17:56 +1100

    Making some components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 
 function App() {
-  return "Hello World"
+  return <div>
+    <h1>Hello World!</h1>
+    <Folder name="Desktop"/>
+    <Folder name="Application"/>
+
+  </div>
+}
+
+const Folder = (props) => {
+  return <h4>{props.name}</h4>
 }
 
 export default App;
