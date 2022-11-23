615f861bf2ab6fb2636283895c130001e77e66c8
dkdam <dkdam@live.com>
2022-11-23 18:31:00 +1100

    added some hooks to toggle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React, { useState } from "react"
 
 function App() {
   return <div>
-    <Folder name = "Desktop">
+
+    <Folder name = "Desktop" isOpen = {false}>
+      <Folder name = "Music">
+        <File name = "Some_Song.mp3" />
+        <File name = "More_Song.mp4" />
+      </Folder>
       <File name = "dog.png" />
       <File name = "cat.png" />
       <File name = "bird.png" />
@@ -13,14 +19,22 @@ function App() {
 }
 
 const Folder = (props) => {
-  console.log(props)
+  const [ isOpen, setIsOpen ] = useState(true)
+  const { name, children } = props
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
   return <div>
-    {props.name}
-    {props.children}
+      <span onClick={handleClick}>{name}</span>
+      <div style={{ margin : '17px'}}>
+        {isOpen ? children : null}
+      </div>
     </div>
 }
 
 const File = (props) => {
+
   return <h5>{props.name}</h5>
 }
 
